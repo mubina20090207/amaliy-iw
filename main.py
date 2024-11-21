01c3aac779ef2eba36d9efc6e40799250fb60528
Alijonov <alijonov.me@gmail.com>
2024-11-21 17:42:08 +0500

    database

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print('Hello World')
+import psycopg2
+
+
+class DataBase:
+    def __init__(self):
+        self.database = psycopg2.connect(
+            database='school',
+            user='postgres',
+            host='localhost',
+            password='1'
+        )
+
+    def manager(self, sql, *args, commit=False, fetchone=False, fetchall=False):
+        with self.database as db:
+            with db.cursor() as cursor:
+                cursor.execute(sql, args)
+                if commit:
+                    result = db.commit()
+                elif fetchone:
+                    result = cursor.fetchone()
+                elif fetchall:
+                    result = cursor.fetchall()
+            return result
